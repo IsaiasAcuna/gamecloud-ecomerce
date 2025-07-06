799b723e5a8c342297ef25bb5664b2071c0c2ce6
fernando-ernesto <fernandoeherrera@outlook.com>
2025-07-06 15:16:01 -0300

    Agregué carpeta e-commerce-gamecloud con mis componentes

---
+++ e-commerce-gamecloud/Appgamecloud.jsx
@@ -0,0 +1,28 @@
+
+
+import Main from './Main';
+import Header from './Header';
+import Footer from './Footer';
+import Contacts from './Contacts';
+
+
+
+
+
+
+const Appgamecloud = () => {
+  return (
+    <>
+      <div>
+        
+          <Header />
+          <Main/>
+          <Contacts />
+          <Footer />
+      
+      </div>
+    </>
+  )
+}
+
+export default Appgamecloud
